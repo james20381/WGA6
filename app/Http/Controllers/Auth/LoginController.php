21362aef24bd4f088e02932a1df1cc00e95e1c3a
James20381 <pikej307@gmail.com>
2018-02-28 23:45:07 +1100

    Install and Setup Laratrust. Re-organise Laravel Auth Files.

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -25,7 +25,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/authDashboard';
 
     /**
      * Create a new controller instance.
--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/authDashboard';
 
     /**
      * Create a new controller instance.
